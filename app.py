c06003c906dd7c93fd0834c6f7f312ae10ca9ada
Mohnish2004 <mohnishgopi@gmail.com>
2023-04-08 01:42:51 -0700

    adding html page as well as templates directory

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('base.html')
+
+@app.route('/about')
+def about():
+    return "About"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
