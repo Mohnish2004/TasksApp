1265808a8117f034625c45969b4c8beec85dd179
Mohnish2004 <mohnishgopi@gmail.com>
2023-04-14 20:33:38 -0700

    Completed basic / implementing slider

--- app.py
+++ app.py
@@ -3,39 +3,41 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' # /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # To ensure there are no warnings
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+class Todo(db.Model): # this is the database model, where we specify all entries for the database
+    id = db.Column(db.Integer, primary_key=True) # This database needs 3 things, id, to do title and boolean value to know if its completed 
+    title = db.Column(db.String(100)) # straight forward strng for title with max characters 100
+    complete = db.Column(db.Boolean) # boolean value to check completion
+    status = db.Column(db.Integer) # EXPERIMENTAL TO ADD STATUS SLIDER
+
 
 
 @app.route("/")
-def home():
-    todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
+def homepage():
+    todo_list = Todo.query.all() # shows all tasks
+    return render_template("base.html", todo_list=todo_list) # flask depends on jinja engine which is used in html file
 
 
-@app.route("/add", methods=["POST"])
-def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
+@app.route("/add", methods=["POST"])# this array is a post method, need to learn it
+def add(): # to add new to do 
+    title = request.form.get("title") # also request is imported, this gets the title from the html file
+    new_todo = Todo(title=title, complete=False) 
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("homepage")) #redirect,url form are imported 
+    # this refreshes the page when user adds item 
 
 
-@app.route("/update/<int:todo_id>")
+@app.route("/update/<int:todo_id>") # this is different because its for an integer id 
 def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
+    todo = Todo.query.filter_by(id=todo_id).first() # we are not just requested but instead querying/searching for the update function. THe first is to return just one
+    todo.complete = not todo.complete # changes boolean value
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("homepage"))
 
 
 @app.route("/delete/<int:todo_id>")
@@ -43,9 +45,17 @@ def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("homepage"))
+
+@app.route("/status/<int:todo_id>", methods=["POST"]) # this is different because its for an integer id 
+def status(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first() # we are not just requested but instead querying/searching for the update function. THe first is to return just one
+    todo.status = int(request.form["status"]) # changes depending on slider
+    db.session.commit()
+    return redirect(url_for("homepage"))
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    with app.app_context(): # creates context for DB to be created
+        db.drop_all()
+        db.create_all() # creates database - this resulted in database file being created 
     app.run(debug=True)
